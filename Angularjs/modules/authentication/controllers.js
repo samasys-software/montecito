85c3700541513a7ba70866de6d4de8139536344e
kathirvelnatarajan <30907665+kathirvelnatarajan@users.noreply.github.com>
2018-01-14 00:08:09 -0500

    login-page

---
+++ Angularjs/modules/authentication/controllers.js
@@ -0,0 +1,23 @@
+'use strict';
+ 
+angular.module('Authentication')
+ 
+.controller('LoginController',
+    ['$scope', '$rootScope', '$location', 'AuthenticationService',
+    function ($scope, $rootScope, $location, AuthenticationService) {
+        // reset login status
+        AuthenticationService.ClearCredentials();
+ 
+        $scope.login = function () {
+            $scope.dataLoading = true;
+            AuthenticationService.Login($scope.username, $scope.password, function(data) {
+                if(!data.error) {
+                    AuthenticationService.SetCredentials(data.firstName, data.lastName, data.token);
+                    $location.path('/');
+                } else {
+                    $scope.error = response.message;
+                    $scope.dataLoading = false;
+                }
+            });
+        };
+    }]);
