37ce36d1d9ff5b1a4d5aea49ca42b2869a247f0a
DhayaKDS <30907761+DhayaKDS@users.noreply.github.com>
2018-01-16 22:23:28 -0500

    Login Screen and Home page
    Login Screen
    Home page -  list view and bar chart with update values

--- Android/app/src/main/java/com/montecito/samayu/service/MontecitoService.java
+++ Android/app/src/main/java/com/montecito/samayu/service/MontecitoService.java
@@ -1,10 +1,16 @@
 package com.montecito.samayu.service;
 
+import com.dto.ConsumptionInfo;
+import com.dto.ItemAvailabilityDTO;
 import com.dto.LoginDTO;
+
+import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.http.Field;
 import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.GET;
+import retrofit2.http.Header;
 import retrofit2.http.POST;
 
 /**
@@ -15,5 +21,11 @@ public interface MontecitoService {
 @POST("montecito/auth/local")
     @FormUrlEncoded
     public Call<LoginDTO> authenticate(@Field("userId") String userId, @Field("password")String password);
+
+    @GET("montecito/api/items/consumption/today/category")
+    public Call<List<ConsumptionInfo>> getConsumptionInfo(@Header("Authorization") String token);
+
+    @GET("montecito/api/tasks/user")
+    public Call<List<ItemAvailabilityDTO>> getItemAvailablityDTO(@Header("Authorization") String token);
 
 }
