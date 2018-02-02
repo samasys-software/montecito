8bc1f9f1b4ebc9ea8ba0b2043ba9a71166d3ff24
Nandhini Govindasamy <nandinigovi143@gmail.com>
2018-02-02 06:08:59 -0500

    itemBinDTO Changes

---
+++ Android/app/src/main/java/com/montecito/samayu/dto/ReadingValueDTO.java
@@ -0,0 +1,16 @@
+package com.montecito.samayu.dto;
+
+/**
+ * Created by NandhiniGovindasamy on 2/2/18.
+ */
+public class ReadingValueDTO {
+    public String getWeight() {
+        return weight;
+    }
+
+    public void setWeight(String weight) {
+        this.weight = weight;
+    }
+
+    public String weight;
+}
