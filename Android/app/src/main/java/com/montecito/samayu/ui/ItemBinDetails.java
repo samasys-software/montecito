d2873a1fa0d694f50b4cd54e7a6ad00b764ab40a
Nandhini Govindasamy <nandinigovi143@gmail.com>
2018-02-03 02:32:02 -0500

    gradient changes

--- Android/app/src/main/java/com/montecito/samayu/ui/ItemBinDetails.java
+++ Android/app/src/main/java/com/montecito/samayu/ui/ItemBinDetails.java
@@ -9,6 +9,9 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import com.github.aakira.expandablelayout.ExpandableRelativeLayout;
 import com.montecito.samayu.dto.ItemBinDTO;
@@ -28,33 +31,54 @@ public class ItemBinDetails extends MontecitoBaseActivity {
         itemButton=findViewById(R.id.itemButton);
         alertButton=findViewById(R.id.alertButton);
         binItems= SessionInfo.getInstance().getCurrentItem();
+        binDetails(false);
         binButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                BinDetails(view);
+                binDetails();
 
             }
         });
         itemButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                ItemDetails(view);
+                ItemDetails();
 
             }
         });
         alertButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-              AlertDetails(view);
+              AlertDetails();
 
             }
         });
 
 
     }
-    public void BinDetails(View view) {
+    public void binDetails (){
+        binDetails(true);
+    }
+    public void binDetails(boolean toggle) {
         expandableLayout1 = (ExpandableRelativeLayout) findViewById(R.id.BinDetailsLayout);
-        expandableLayout1.toggle(); // toggle expand and collapse
+        if(toggle) expandableLayout1.toggle(); // toggle expand and collapse
+        //RelativeLayout relativeLayout=(RelativeLayout) expandableLayout1.findViewById(R.id.bin);
+        TextView binName=(TextView) findViewById(R.id.binName);
+        TextView binLocation=(TextView) findViewById(R.id.binLocation);
+        TextView binType=(TextView) findViewById(R.id.binType);
+        TextView binDimension=(TextView) findViewById(R.id.binDimension);
+        TextView cBinIdentity=(TextView) findViewById(R.id.cBinIdentity);
+        TextView rfid=(TextView) findViewById(R.id.rfid);
+
+        binName.setText(binItems.getCrateBin().getName());
+        binLocation.setText(binItems.getCurrDevice().getLocation());
+        binType.setText("-----");
+        binDimension.setText("-----");
+        rfid.setText("-----");
+        cBinIdentity.setText(binItems.getCurrDevice().getName());
+
+
+
        if(expandableLayout1.isExpanded())
        {
            binButton.setImageResource(R.drawable.downarrow);
@@ -65,9 +89,25 @@ public class ItemBinDetails extends MontecitoBaseActivity {
        }
     }
 
-    public void ItemDetails(View view) {
+    public void ItemDetails() {
         itemDetailsLayout = (ExpandableRelativeLayout) findViewById(R.id.ItemDetailsLayout);
         itemDetailsLayout.toggle(); // toggle expand and collapse
+        TextView itemName=(TextView) findViewById(R.id.itemName);
+        TextView itemDimension=(TextView) findViewById(R.id.itemDimension);
+        TextView itemVolume=(TextView) findViewById(R.id.itemVolume);
+        TextView material=(TextView) findViewById(R.id.material);
+        TextView units=(TextView) findViewById(R.id.units);
+        TextView surface=(TextView) findViewById(R.id.surface);
+        TextView availability=(TextView) findViewById(R.id.availability);
+
+        itemName.setText(binItems.getItem().getName());
+        material.setText(binItems.getItem().getMaterial());
+        itemDimension.setText("-----");
+        itemVolume.setText("------");
+        units.setText(binItems.getItem().getUom());
+        surface.setText("------");
+        availability.setText("-----");
+
         if(itemDetailsLayout.isExpanded())
         {
             itemButton.setImageResource(R.drawable.downarrow);
@@ -78,9 +118,10 @@ public class ItemBinDetails extends MontecitoBaseActivity {
         }
     }
 
-    public void AlertDetails(View view) {
+    public void AlertDetails() {
         alertSettingsLayout = (ExpandableRelativeLayout) findViewById(R.id.AlertSettingsLayout);
         alertSettingsLayout.toggle(); // toggle expand and collapse
+
         if(alertSettingsLayout.isExpanded())
         {
             alertButton.setImageResource(R.drawable.downarrow);
